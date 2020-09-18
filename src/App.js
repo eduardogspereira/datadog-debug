8fc56d58d3fc8a64e2e13db0e2c898b568263c74
Eduardo G. S. Pereira <edu_vcd@hotmail.com>
2020-09-18 11:16:05 -0300

    wip

--- src/App.js
+++ src/App.js
@@ -1,26 +1,59 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter, HashRouter, Route, Switch } from "react-router-dom";
 
-function App() {
+const ChangeRoute = ({ history }) => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <button onClick={() => history.push("route-a")}>Rota A</button>
+      <button onClick={() => history.push("route-b")}>Rota B</button>
+      <button onClick={() => history.push("route-c")}>Rota C</button>
+      <button onClick={() => history.push("route-d")}>Rota D</button>
     </div>
   );
-}
+};
 
-export default App;
+/**
+ * Can't track route changes with HashRouter.
+ */
+const FirstScenario = () => {
+  window.addEventListener(
+    "hashchange",
+    (l) => console.log(window.location.hash),
+    false
+  );
+
+  return (
+    <div className="App">
+      <HashRouter>
+        <Switch>
+          <Route component={ChangeRoute} path="/route-a" />
+          <Route component={ChangeRoute} path="/route-b" />
+          <Route component={ChangeRoute} path="/route-c" />
+          <Route component={ChangeRoute} path="/route-d" />
+          <Route component={ChangeRoute} path="/" />
+        </Switch>
+      </HashRouter>
+    </div>
+  );
+};
+
+/**
+ * Can't track route changes when basename is set to #.
+ */
+const SecondScenario = () => {
+  return (
+    <div className="App">
+      <BrowserRouter basename="#">
+        <Switch>
+          <Route component={ChangeRoute} path="/route-a" />
+          <Route component={ChangeRoute} path="/route-b" />
+          <Route component={ChangeRoute} path="/route-c" />
+          <Route component={ChangeRoute} path="/route-d" />
+          <Route component={ChangeRoute} path="/" />
+        </Switch>
+      </BrowserRouter>
+    </div>
+  );
+};
+
+export default FirstScenario;
--- src/index.js
+++ src/index.js
@@ -1,17 +1,16 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import { datadogRum } from "@datadog/browser-rum";
+import App from "./App";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+datadogRum.init({
+  applicationId: "REDACTED",
+  clientToken: "REDACTED",
+  sampleRate: 100,
+  service: "debug",
+  site: "datadoghq.com",
+  trackInteractions: true,
+  version: "1",
+});
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+ReactDOM.render(<App />, document.getElementById("root"));
